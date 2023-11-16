6c0e8f76a89e232659926ce6a766fadc776f96ff
Islam Boubakeur <islamboubakeur@hotmail.com>
2023-11-16 01:20:59 +0100

    Add init and manifest

---
+++ custom/estate/__manifest__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'estate',
+    'depends': [ 'base' ],
+    'application': True,
+}
--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 from os import system as sys
 
-sys("python odoo\odoo-bin -c odoo.conf --dev xml")
+sys("python odoo\odoo-bin -c odoo.conf --update estate --dev xml")
